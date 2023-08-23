f18957824ec5cf4eb4470f18fc9bca86e192f86f
MrPhoneiX <100272193+MrPhoneiX@users.noreply.github.com>
2023-08-23 17:28:27 +0500

    deployment
    Railway.app - deploy uchun

---
+++ news_app/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Contact, Comment
+
+
+class ContactForm(forms.ModelForm):
+
+    class Meta:
+        model = Contact
+        fields = '__all__'
+
+class SubscriptionForm(forms.Form):
+    pass
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['body']
+
